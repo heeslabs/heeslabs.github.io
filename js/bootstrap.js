baa9ed11ddb24f322e6b7241f1dd14cfa41315bc
heeslabs <77578625+heeslabs@users.noreply.github.com>
2024-05-14 16:40:17 +0900

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1867,25 +1867,6 @@ if (typeof jQuery === 'undefined') {
     this.targets = []
     this.scrollHeight = this.getScrollHeight()
 
-    var self     = this
-
-    this.$body
-      .find(this.selector)
-      .map(function () {
-        var $el   = $(this)
-        var href  = $el.data('target') || $el.attr('href')
-        var $href = /^#./.test(href) && $(href)
-
-        return ($href
-          && $href.length
-          && $href.is(':visible')
-          && [[$href[offsetMethod]().top + offsetBase, href]]) || null
-      })
-      .sort(function (a, b) { return a[0] - b[0] })
-      .each(function () {
-        self.offsets.push(this[0])
-        self.targets.push(this[1])
-      })
   }
 
   ScrollSpy.prototype.process = function () {
